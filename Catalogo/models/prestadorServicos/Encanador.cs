ac2c8531cc821a8b83badf431c0f947208694a9d
Elder <eldercnw@gmail.com>
2020-05-16 15:52:30 -0300

    Encanador

--- Catalogo/models/prestadorServicos/Encanador.cs
+++ Catalogo/models/prestadorServicos/Encanador.cs
@@ -8,15 +8,13 @@ namespace Catalogo.models.prestadorServicos
 {
     class Encanador : PrestadorDeServicos
     {
-<<<<<<< HEAD
+
         public static List<Encanador> listaEncanadores = new List<Encanador>();
 
         public override string ToString()
         {
             return $"Categoria: {Cliente.GetTipo(this)}\nNome: {this.nome}\n CPF: {this.cpf}\n\n";
         }
-=======
-        public static List<Encanador> listaEncadores = new List<Encanador>();
->>>>>>> 7ee8ec4d064210f94df7f4f5c9a0b2f6501b803d
+
     }
 }
